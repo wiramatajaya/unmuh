d7e5e7dc0b0a880dd608b2fdaa7f7a00cec809f0
jek <wiramatajaya10@gmail.com>
2025-04-19 11:42:55 +0700

    update

--- oop/Ridok/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        Laptop newLaptop = new Laptop(" MITO ", " intel core i20 ", 256);
-        System.out.println(newLaptop.Check());
-    }
-}
